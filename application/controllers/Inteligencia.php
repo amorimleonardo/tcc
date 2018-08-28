16d858312b9afb61bcecb01d059284225d1272e3
Leonardo Amorim <leonardo@upecommerce.com.br>
2018-08-28 07:32:38 -0300

    modulo de inteligencia artificial

---
+++ application/controllers/Inteligencia.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Inteligencia extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+        $this->load->library('php-ml/Intelligence');
+        $this->Intelligence = new Intelligence();
+	}
+
+	public function index()
+	{
+		$quantity = [[1], [2], [3], [4], [5]];
+		$date = [1369526400, 1372204800, 1374796800, 1377475200, 1380153600];
+
+		echo $this->Intelligence->predicao($quantity, $date);exit();
+
+		$this->load->view('header');
+		$this->load->view('home');
+		$this->load->view('footer');
+	}
+}
