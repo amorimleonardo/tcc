6c8cfbf6c32ed206c05f9048ef66b4ce533dfa0a
Leonardo Amorim <leonardo@upecommerce.com.br>
2018-09-05 18:28:46 -0300

    bd

--- sql.sql
+++ sql.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.7.21 - MySQL Community Server (GPL)
+-- Versão do servidor:           5.7.14 - MySQL Community Server (GPL)
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              9.4.0.5125
+-- HeidiSQL Versão:              9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,7 +26,10 @@ CREATE TABLE IF NOT EXISTS `agrup_usuario` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.agrup_usuario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `agrup_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `agrup_usuario` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.balanca
 DROP TABLE IF EXISTS `balanca`;
 CREATE TABLE IF NOT EXISTS `balanca` (
@@ -36,18 +39,29 @@ CREATE TABLE IF NOT EXISTS `balanca` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.balanca: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `balanca` DISABLE KEYS */;
+/*!40000 ALTER TABLE `balanca` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.compra
 DROP TABLE IF EXISTS `compra`;
 CREATE TABLE IF NOT EXISTS `compra` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `id_user` mediumint(9) DEFAULT NULL,
-  `id_produto` mediumint(9) DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `id_produto` int(11) DEFAULT NULL,
   `data_compra` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `FK_compra_produto` (`id_produto`),
+  CONSTRAINT `FK_compra_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.compra: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+INSERT INTO `compra` (`id`, `id_user`, `id_produto`, `data_compra`) VALUES
+	(8, 2, 13, '2018-09-05'),
+	(9, 2, 14, '2018-09-05');
+/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.historico
 DROP TABLE IF EXISTS `historico`;
 CREATE TABLE IF NOT EXISTS `historico` (
@@ -57,7 +71,10 @@ CREATE TABLE IF NOT EXISTS `historico` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.historico: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `historico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historico` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.mercado
 DROP TABLE IF EXISTS `mercado`;
 CREATE TABLE IF NOT EXISTS `mercado` (
@@ -66,7 +83,10 @@ CREATE TABLE IF NOT EXISTS `mercado` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.mercado: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `mercado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mercado` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.produto
 DROP TABLE IF EXISTS `produto`;
 CREATE TABLE IF NOT EXISTS `produto` (
@@ -75,9 +95,16 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `qtd_min` float(4,2) DEFAULT NULL,
   `preco_custo` float(4,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.produto: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` (`id`, `nome`, `qtd_min`, `preco_custo`) VALUES
+	(13, 'arroz', NULL, 5.30),
+	(14, 'feijão', NULL, 10.60),
+	(15, 'açucar', NULL, 7.30);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.prox_compra
 DROP TABLE IF EXISTS `prox_compra`;
 CREATE TABLE IF NOT EXISTS `prox_compra` (
@@ -88,19 +115,28 @@ CREATE TABLE IF NOT EXISTS `prox_compra` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.prox_compra: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `prox_compra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prox_compra` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela tcc.usuarios
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
+  `dt_nasc` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela tcc.usuarios: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`id`, `name`, `email`, `password`, `created`, `dt_nasc`) VALUES
+	(2, 'leo', 'leo@teste.com', '123', NULL, '2014-11-30 00:00:00');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
